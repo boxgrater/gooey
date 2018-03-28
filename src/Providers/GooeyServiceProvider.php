6a1ab223af8875d75ccc1bc834d4a2b356b314a4
Frank McCoy <frankmccoy.d@gmail.com>
2018-03-28 11:26:36 -0500

    gooey init

---
+++ src/Providers/GooeyServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BoxGrater\Gooey\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class GooeyServiceProvider extends ServiceProvider
+{
+  public function boot()
+  {
+    //
+  }
+
+  public function register()
+  {
+    //
+  }
+}
